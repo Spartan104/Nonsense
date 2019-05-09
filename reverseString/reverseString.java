9bf0f74b789871f2af4554d6fe1f4ee2b5606a92
Cole Roof <ckroof@gmail.com>
2019-05-09 09:22:07 -0400

    I fixed your code Lucas! - <3 Cole
    I'm pretty sure it works now

--- reverseString/reverseString.java
+++ reverseString/reverseString.java
@@ -1,20 +1,27 @@
 import java.util.Scanner;
 public class reverseString {
-    //Why am I commenting again?
+
+    // Why am I commenting again?
+    // Because it's useful for ME!!! -Cole
     public static void main(String[] args)
     {
         Scanner scan = new Scanner( System.in );
-        System.out.print( "Please input your name );
-        string name = scan.nextLine();
-        System.out.print( ReverseName(name) );
+        System.out.print( "Please input your name" );
+        String name = scan.nextLine();
+        System.out.println( "Your name, reveresed, is: " + reverseName(name) );
     }
-    //Helper methods that don't do anything!
-    private static String ReverseName(String s)
+
+
+    // Helper methods that don't do anything!
+    // We should probably use StringBuilder, but... oh well...
+    private static String reverseName(String s)
     {
         String rs = "";
-        for ( int i = 0; i < s.length; i++ )
+        // Start at the end of the string and go to the front
+        for ( int i = s.length - 1; i >= 0 ; i-- )
         {
-            rs += s.charAt(s.length() - i);
+            // Append the char to the new string
+            rs += s.charAt(i);
         }
         return rs;
     }
